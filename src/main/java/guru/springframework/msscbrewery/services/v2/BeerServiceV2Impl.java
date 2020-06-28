169878c10974e07390e28b6e76a1e95a1986fc2f
Oleksandr Sytnik <alexandr.sytnik@gmail.com>
2020-06-28 21:22:39 +0200

    Added the second version of the API (the Beer DTO uses an enum to represent the beer style)

---
+++ src/main/java/guru/springframework/msscbrewery/services/v2/BeerServiceV2Impl.java
@@ -0,0 +1,42 @@
+package guru.springframework.msscbrewery.services.v2;
+
+import guru.springframework.msscbrewery.web.model.v2.BeerDtoV2;
+import guru.springframework.msscbrewery.web.model.v2.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+/**
+ * Created by jt on 2019-04-20.
+ */
+@Slf4j
+@Service
+public class BeerServiceV2Impl implements BeerServiceV2 {
+    @Override
+    public BeerDtoV2 getBeerById(UUID beerId) {
+        return BeerDtoV2.builder()
+                .id(UUID.randomUUID())
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyle.IPA)
+                .build();
+    }
+
+    @Override
+    public BeerDtoV2 saveNewBeer(BeerDtoV2 BeerDtoV2) {
+        return BeerDtoV2.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDtoV2 BeerDtoV2) {
+        // TODO: implement it
+        log.debug("Updating a beer...");
+    }
+
+    @Override
+    public void delete(UUID beerId) {
+        log.debug("Deleting a beer...");
+    }
+}
---
+++ src/main/java/guru/springframework/msscbrewery/web/model/v2/BeerStyle.java
@@ -0,0 +1,10 @@
+package guru.springframework.msscbrewery.web.model.v2;
+
+public enum BeerStyle {
+    LAGER,
+    PILSNER,
+    ALE,
+    STOUT,
+    GOSE,
+    IPA
+}
