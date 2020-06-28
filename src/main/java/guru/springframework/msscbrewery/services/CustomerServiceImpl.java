915a186c11921a78ddee751ae44ce2d487eb8e43
Oleksandr Sytnik <alexandr.sytnik@gmail.com>
2020-06-28 16:59:48 +0200

    Added a basic implementation of customers related functionality

---
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -0,0 +1,17 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.CustomerDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public CustomerDTO getCustomerById(UUID id) {
+        return CustomerDTO.builder()
+                .id(UUID.randomUUID())
+                .name("Customer name")
+                .build();
+    }
+}
