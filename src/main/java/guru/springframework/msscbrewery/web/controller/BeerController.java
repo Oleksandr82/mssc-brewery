724499f84ac148a675a5257e441f12a51d51cceb
Oleksandr Sytnik <alexandr.sytnik@gmail.com>
2020-06-29 17:58:53 +0200

    Added constraint validation and exception handling.

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -5,10 +5,14 @@ import guru.springframework.msscbrewery.web.model.BeerDto;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * Created by jt on 2019-04-20.
@@ -53,5 +57,21 @@ public class BeerController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteBeer(@PathVariable("beerId") UUID beerId) {
         beerService.delete(beerId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<List> methodArgumentsNotValidErrorHandler(MethodArgumentNotValidException e) {
+        List<String> errors = e.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getObjectName() + "." + error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e) {
+        List<String> errors = e.getConstraintViolations().stream()
+                .map(violation -> violation.getPropertyPath() + ": " + violation.getMessage())
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
+++ src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
 import java.util.UUID;
 
 @Data
@@ -13,6 +17,11 @@ import java.util.UUID;
 @Builder
 public class CustomerDto {
 
+    @Null
     private UUID id;
+
+    @NotBlank
+    @Min(3)
+    @Max(100)
     private String name;
 }
