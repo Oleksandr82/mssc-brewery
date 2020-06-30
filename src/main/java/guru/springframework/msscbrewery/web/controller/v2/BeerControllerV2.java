735150eb1effc355322aa9525554c353c40405d0
Oleksandr Sytnik <alexandr.sytnik@gmail.com>
2020-06-30 13:08:02 +0200

    Added mapping between BeerDTO and the Beer object using MapStruct

--- src/main/java/guru/springframework/msscbrewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/v2/BeerControllerV2.java
@@ -2,6 +2,8 @@ package guru.springframework.msscbrewery.web.controller.v2;
 
 import guru.springframework.msscbrewery.services.v2.BeerServiceV2;
 import guru.springframework.msscbrewery.web.model.v2.BeerDtoV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,15 +14,14 @@ import java.util.UUID;
 /**
  * Created by jt on 2019-04-20.
  */
+@Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/api/v2/beer")
 @RestController
 public class BeerControllerV2 {
 
     private final BeerServiceV2 beerService;
 
-    public BeerControllerV2(BeerServiceV2 beerService) {
-        this.beerService = beerService;
-    }
 
     @GetMapping({"/{beerId}"})
     public ResponseEntity<BeerDtoV2> getBeer(@PathVariable("beerId") UUID beerId) {
@@ -30,6 +31,8 @@ public class BeerControllerV2 {
 
     @PostMapping
     public ResponseEntity<Void> handlePost(@RequestBody BeerDtoV2 BeerDtoV2) {
+
+        log.debug("handling post...");
 
         BeerDtoV2 savedDto = beerService.saveNewBeer(BeerDtoV2);
 
