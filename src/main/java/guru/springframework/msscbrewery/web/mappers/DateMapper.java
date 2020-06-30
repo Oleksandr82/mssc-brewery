8b5dc1a1ba046454ea21b504a133b9643dd9fe1c
Oleksandr Sytnik <alexandr.sytnik@gmail.com>
2020-06-30 13:32:40 +0200

    Extended BeerDto and Beer with timestamps and added custom convertors for dates

---
+++ src/main/java/guru/springframework/msscbrewery/web/mappers/DateMapper.java
@@ -0,0 +1,27 @@
+package guru.springframework.msscbrewery.web.mappers;
+
+import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
+@Component
+public class DateMapper {
+
+    public OffsetDateTime asOffsetDateTime(Timestamp timestamp) {
+        if (timestamp == null) {
+            return null;
+        }
+        return OffsetDateTime.ofInstant(timestamp.toInstant(), ZoneId.of("UTC"));
+    }
+
+    public Timestamp asTimestamp(OffsetDateTime offsetDateTime) {
+        if (offsetDateTime == null) {
+            return null;
+        }
+        return Timestamp.valueOf(LocalDateTime.ofInstant(offsetDateTime.toInstant(), ZoneOffset.UTC));
+    }
+}
